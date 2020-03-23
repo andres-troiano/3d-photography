5e90d39f119cf3fd2e17414b0789ba6114af0628
Andrés Troiano <61122404+andres-troiano@users.noreply.github.com>
2020-03-23 20:48:08 -0300

    Update fronteras_region_valida.m

--- fronteras_region_valida.m
+++ fronteras_region_valida.m
@@ -1,8 +1,7 @@
-% script para generar las fronteras de la regiÛn donde queremos trabajar
-% (rosca determinada por el radio del patrÛn 630 +- 50 mm).
-% Para calcular el centro del patrÛn necesito primero tener una
-% calibraciÛn. Uso la del trapecio, que ya la tengo, pero no sÈ si es lo
-% mejor.
+% script para generar las fronteras de la regi√≥n donde queremos trabajar
+% (rosca determinada por el radio del patr√≥n "630" +- 50 mm).
+% Para calcular el centro del patr√≥n necesito primero tener una
+% calibraci√≥n. Uso la del trapecio.
 
 clear variables
 
@@ -21,8 +20,8 @@ XY = {[], []};
 % coordenadas de los puntos centrales de cada perfil, en mm. En columnas:
 % xc;yc
 M = {nan(2,1), nan(2,1)};
-% este valor me sirve para separar los datos de cada c·mara despuÈs de
-% haberlos juntado en XY, asÌ no los tengo que volver a procesar
+% este valor me sirve para separar los datos de cada c√°mara despu√©s de
+% haberlos juntado en XY, as√≠ no los tengo que volver a procesar
 s = nan; 
 for q = 1:2
     frame = imread(fullfile(path_datos, frames{q}));
@@ -49,8 +48,8 @@ for q = 1:2
         s = numel(py);
     end
     
-    % ubico Ìndice correspondiente al mÌnimo en pÌxels, para quedarme sÛlo
-    % con el arco central
+    % ubico √≠ndice correspondiente al m√°ximo en p√≠xels, que corresponde al
+    % punto central del arco
     [m,i] = max(py);
     
     x = polyval4XY(px2mmPol{q}(1), px, py);
@@ -67,25 +66,29 @@ for q = 1:2
     yc = y(i);
     M{q}(:) = [xc;yc];
     
-    plot(x,y,'.')
+    if q == 1
+        plot(x,y,'ob')
+    end
+    
+    if q == 2
+        plot(x,y,'.r')
+    end
+    
     axis equal
     grid on
     xlabel('X (mm)')
     ylabel('Y (mm)')
     
-    % guardo los datos en una celda que junta las 2 c·maras
+    % guardo los datos en una celda que junta las 2 c√°maras
     XY{1} = [XY{1}; x];
     XY{2} = [XY{2}; y];
-    
-%     plot(px,py,'.')
-%     plot(px(i), py(i), 'o')
 
 end
 
 % convierto la celda a matriz
 XY = [XY{1}, XY{2}];
 
-% veo quÈ tan circular es, y mido el di·metro
+% veo qu√© tan circular es, y mido el di√°metro
 circulo = TaubinNTN(XY);
 
 centro_x = circulo(1);
@@ -96,25 +99,18 @@ t = linspace(0, 2*pi, 100);
 x_teorico = r_teorico * cos(t) + centro_x;
 y_teorico = r_teorico * sin(t) + centro_y;
 
-plot(x_teorico, y_teorico, '--r')
+% plot(x_teorico, y_teorico, '--r')
 plot(centro_x, centro_y, '+r')
 plot(M{1}(1), M{1}(2), 'og')
 plot(M{2}(1), M{2}(2), 'om')
 
 %%
 
-% ahora que tengo la posiciÛn del centro del sistema, armo la m·scara que
+% ahora que tengo la posici√≥n del centro del sistema, armo la m√°scara que
 % necesito. Se compone de 2 cosas:
-% 1) mirar sÛlo un rango de ·ngulos
-% 2) mirar sÛlo un rango de radios
+% 1) mirar s√≥lo un rango de √°ngulos
+% 2) mirar s√≥lo un rango de radios
 
-% ·ngulos:
-% para filtrar cada perfil por separado, necesito tenerlos separados
-% justamente. O hacer otro loop sobre los perfiles
-
-% calculo el ·ngulo del segmento central (respecto de la vertical)
-
-close all, figure, hold on, grid on
 for q = 1:2
     
     if q == 1
@@ -132,65 +128,81 @@ for q = 1:2
     alpha_izq = alpha + 35;
     alpha_der = alpha - 35;
     
-%     a = 1/tand(alpha);
-    a = tand(alpha+90); % esto lo emparchÈ a mano
+    a = tand(alpha+90); % esto lo emparch√© a mano
     y_recta = a*x + (centro_y - a*centro_x);
 
-%     a = 1/tand(alpha_izq);
     a = tand(alpha_izq+90);
     y_recta_izq = a*x + (centro_y - a*centro_x);
 
-%     a = 1/tand(alpha_der);
     a = tand(alpha_der+90);
     y_recta_der = a*x + (centro_y - a*centro_x);
     
-    plot(x, y, '.')
-    plot(x, y_recta, '--k')
-    plot(M{q}(1), M{q}(2), '+r')
     plot(centro_x, centro_y, '+b')
-    plot(x, y_recta_izq, '--b')
-    plot(x, y_recta_der, '--r')
-    axis equal
     
-%     % no es lo mismo si la "derecha" tiene pendiente positiva que
-%     % negativa
-%     if a < 0
-%         flag_a = -1;
-%     end
-% 
-%     if a > 0
-%         flag_a = 1;
-%     end
-% 
-%     % me quedo con los puntos que est·n dentro de los 70∫
-%     % ATENCI”N! Esto lo voy a hacer en dos partes: comparo con 1 recta y
-%     % guardo, comparo con la otra y guardo en otro lado, y al final junto
-%     filtro = y < y_recta_izq & y < y_recta_der;
-%     x1 = x(filtro);
-%     y1 = y(filtro);
-% 
-%     if flag_a == -1
-%         filtro = y > y_recta_der & y < y_recta_izq;
-%     end
-% 
-%     if flag_a == 1
-%         filtro = y < y_recta_der & y < y_recta_izq;
-%     end
-% 
-%     x2 = x(filtro);
-%     y2 = y(filtro);
-%     
-%     x = [x1;x2];
-%     y = [y1;y2];
+    if q == 1
+        plot(x, y_recta_izq, '--b')
+        plot(x, y_recta_der, '--b')
+    end
+    
+    if q == 2
+        plot(x, y_recta_izq, '--r')
+        plot(x, y_recta_der, '--r')
+    end
     
 end
 
 axis equal
-xlim([min(XY(:, 1)) max(XY(:, 1))])
-ylim([min(XY(:, 2)) max(XY(:, 2))])
+margen = 50;
+xlim([min(XY(:, 1))-margen max(XY(:, 1))+margen])
+ylim([min(XY(:, 2))-margen max(XY(:, 2))+margen])
 
 %%
 
 % ahora acoto el rango de radios, y al final genero 2 fronteras combinando
 % ambas cosas
-% esto ya est· hecho, hay que ver dÛnde
+
+r_int = r_teorico - 50;
+r_ext = r_teorico + 50;
+
+x_int = centro_x + r_int*cos(t);
+y_int = centro_y + r_int*sin(t);
+
+x_ext = centro_x + r_ext*cos(t);
+y_ext = centro_y + r_ext*sin(t);
+
+plot(x_int, y_int, '--k')
+plot(x_ext, y_ext, '--k')
+
+%%
+
+% ahora genero la frontera con todo
+% chequeo que lo estoy haciendo bien con datos de prueba
+
+load([path_calibracion 'intersections.mat']);
+
+for q = 1:2
+
+    ind1=C{q}(:,6)>.4;
+    ind2=C{q}(:,8)>.4;
+    ind3=C{q}(:,7)<100;
+    ind4=C{q}(:,9)<100;
+
+    ind1=~ind1 & ~ind2 & ~ind3 & ~ind4 & ~isnan(C{q}(:,1));
+
+    grilla_mmx = C{q}(ind1,3);
+    grilla_mmy = C{q}(ind1,4);
+
+    if q == 2
+        grilla_mmx = grilla_mmx-offset(1);
+        grilla_mmy = grilla_mmy-offset(2);
+    end
+    
+    if q == 1
+        plot(grilla_mmx,grilla_mmy,'.c')
+    end
+    
+    if q == 2
+        plot(grilla_mmx,grilla_mmy,'.m')
+    end
+    
+end
