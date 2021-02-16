8b55419ebd696af750d0934d3533ba1f91823f37
Andrés Troiano <61122404+andres-troiano@users.noreply.github.com>
2021-02-16 16:26:33 -0300

    Add files via upload

---
+++ tesis/sin_tirar_bordes/calibracion_2021.m
@@ -0,0 +1,194 @@
+% script para hacer una calibraci�n, tomando como input los datos generados
+% por el script "medicion.m" del proyecto "barrido"
+% (con el TRAPECIO)
+
+% la versión "2021" lo que cambia es que no tira puntos en el ajuste de los
+% polinomios, a pedido de Nicolás para presentar la tesis
+
+clear variables
+%datos nuevos
+path_calibracion = '/home/andres/DIRECTORIO TESIS/2021/sin_tirar_bordes/medicion42_2021/';
+path_offset = '/home/andres/DIRECTORIO TESIS/2021/sin_tirar_bordes/medicion43_2021/';
+% 
+% % datos viejos
+% % path_calibracion = 'C:\Users\Norma\Downloads\datos_calibraciones\medicion27\';
+% % path_offset = 'C:\Users\Norma\Downloads\datos_calibraciones\medicion29\';
+% 
+% %%
+% 
+% % clasifico los datos, eliminando aquellas capturas en las que no se vio
+% % nada
+% creo_directorios_2_camaras(path_calibracion);
+% separar_frames_utiles(path_calibracion, 1);
+% separar_frames_utiles(path_calibracion, 2);
+% % ac� estar�a bueno eliminar los archivos originales, para no tenerlos
+% % duplicados
+% convertFiles2DotMatPath(path_calibracion);
+
+%%
+
+% encuentro la posici�n de las esquinas del patr�n
+% calculateIntersectionsPath(path_calibracion);
+
+% adem�s de la zona de inter�s, genero fronteras de la
+% zona que efectivamente pude calibrar (en caso de no haber llenado
+% la zona de inter�s)
+% fronteraZonaEfectiva(path_calibracion);
+
+load([path_calibracion 'intersections.mat']);
+% teniendo las coordenadas de cada esquina en mm y en pixels, calibro el
+% sistema
+calculateCalibration_2021(C, path_calibracion);
+
+%% 
+
+% posterior a haber calibrado (con el trapecio) hay que hacer un barrido
+% con el trapecio puesto horizontal, para determinar delta_x, delta_y
+
+% ojo! Las deltas hay que calcularlas s�lo en las regiones v�lidas. No me
+% acuerdo si actualmente est� implementado as�
+
+% clear variables
+
+% hecho el 2do barrido para calcular el offset, encuentro las esquinas
+
+% creo_directorios_2_camaras(path_offset);
+% separar_frames_utiles(path_offset, 1);
+% separar_frames_utiles(path_offset, 2);
+% convertFiles2DotMatPath(path_offset);
+
+% ac� hay que se�alar: para la cam1, la esquina izquierda, y para la cam2
+% la derecha
+% calculateIntersectionsPath(path_offset);
+
+% ahora a partir de los datos tengo que calcular el offset. 
+offset = calculo_offset(path_offset, path_calibracion);
+
+%%
+
+% antes de calibrar tengo que quedarme s�lo con las regiones de inter�s.
+% Las fronteras est�n generadas en "fronteras_region_valida"
+
+clear variables
+
+path_calibracion = '/home/andres/DIRECTORIO TESIS/2021/sin_tirar_bordes/medicion42_2021/';
+path_offset = '/home/andres/DIRECTORIO TESIS/2021/sin_tirar_bordes/medicion43_2021/';
+
+load([path_calibracion 'intersections.mat']);
+load([path_offset 'offset.mat']);
+load([path_calibracion 'fronteras.mat']);
+load([path_calibracion 'ind_fronteras.mat']);
+
+close all, figure, hold on, grid on
+for q = 1:2
+
+%     ind1=C{q}(:,6)>.4;
+%     ind2=C{q}(:,8)>.4;
+%     ind3=C{q}(:,7)<100;
+%     ind4=C{q}(:,9)<100;
+% 
+%     ind1=~ind1 & ~ind2 & ~ind3 & ~ind4 & ~isnan(C{q}(:,1));
+% 
+%     grilla_mmx = C{q}(ind1,3);
+%     grilla_mmy = C{q}(ind1,4);
+
+    grilla_mmx = C{q}(:,3);
+    grilla_mmy = C{q}(:,4);
+
+    if q == 2
+        grilla_mmx = grilla_mmx-offset(1);
+        grilla_mmy = grilla_mmy-offset(2);
+    end
+   
+%     ind = inpolygon(grilla_mmx, grilla_mmy, F{q}(:,1), F{q}(:,2));
+    
+    if q == 1
+%         plot(grilla_mmx,grilla_mmy,'.b')
+%         plot(grilla_mmx(ind),grilla_mmy(ind),'.c')
+        plot(grilla_mmx(ind_fronteras{q}),grilla_mmy(ind_fronteras{q}),'.c')
+    end
+    
+    if q == 2
+%         plot(grilla_mmx,grilla_mmy,'.r')
+%         plot(grilla_mmx(ind),grilla_mmy(ind),'.m')
+        plot(grilla_mmx(ind_fronteras{q}),grilla_mmy(ind_fronteras{q}),'.m')
+    end
+    
+    plot(F{q}(:,1), F{q}(:,2), '--')
+    title('Trapecio')
+    
+end
+
+axis equal
+xlabel('X (mm)')
+ylabel('Y (mm)')
+
+% ahora que tengo la frontera, vuelvo a calibrar, vuelvo a medir el offset,
+% y mido el cilindro
+calculateCalibration_con_fronteras_2021(C,path_calibracion, ind_fronteras)
+
+% mido el offset nuevamente
+% no me queda claro si tengo que calcular el offset s�lo en la zona v�lida
+% o no. De ser as�, para la c�mara 2 definir�a la zona v�lida cpath_calibracionpath_calibracionomo la que
+% tengo definida hasta ac� menos el offset que tengo hasta ac�, porque en
+% el script de abajo todav�a los datos no est�n desplazados
+offset_fronteras = calculo_offset_con_fronteras_2021(path_offset, path_calibracion);
+
+%% ahora mido los patrones
+
+clear variables, clc
+
+% para trabajar con datos nuevos:
+path_datos = '/home/andres/DIRECTORIO TESIS/2021/sin_tirar_bordes/medicion48_2021/';
+path_fronteras = '/home/andres/DIRECTORIO TESIS/2021/sin_tirar_bordes/medicion42_2021/';
+path_calibracion = '/home/andres/DIRECTORIO TESIS/2021/sin_tirar_bordes/medicion42_2021/';
+path_offset = '/home/andres/DIRECTORIO TESIS/2021/sin_tirar_bordes/medicion43_2021/';
+
+% usando calibración y datos viejos
+% path_datos = 'C:\Users\Norma\Downloads\datos_calibraciones\cilindros_viejo\';
+% path_fronteras = 'C:\Users\Norma\Downloads\datos_calibraciones\medicion42\'; % esto no lo uso, pero se lo paso igual (adentro de la función lo comento)
+% path_calibracion = 'C:\Users\Norma\Downloads\datos_calibraciones\medicion32\';
+% path_offset = 'C:\Users\Norma\Downloads\datos_calibraciones\medicion43\'; % no pertenece a la medición
+
+% load([path_offset 'camara_1.mat']);
+
+% calibracion y offset sin fronteras
+% load([path_calibracion 'calibration.mat']);
+load([path_offset 'offset.mat']);
+
+% calibracion y offset con fronteras
+load([path_calibracion 'calibration_con_fronteras_2021.mat']);
+% load([path_offset 'offset_fronteras.mat']);
+% offset = offset_fronteras;
+
+% load([path_calibracion 'fronteras.mat']);
+load([path_fronteras 'fronteras.mat']);
+load([path_calibracion 'FC.mat']);
+
+% empaqueto lo que ya tengo hecho en el fronteras_region_valida. Ahí mido
+% el patrón del medio
+
+frame_cilindro = {'patron_34700530', 'patron_34700630', 'patron_34700730'};
+id_cilindro = {'34700530', '34700630', '34700730'};
+nominales = [139.707, 168.310, 177.805];
+
+frames_cilindro = {[], []};
+
+for f = 1:3
+    close all
+    for q = 1:2
+        frames_cilindro{q} = [path_datos frame_cilindro{f} '_camara_' num2str(q) '.png'];
+    end
+    [centro_x, centro_y, r_teorico, r_individual, centro_individual] = mido_patron_2021(frames_cilindro, id_cilindro{f}, px2mmPol, offset, F, FC, [path_datos 'medicion/']); % con las fronteras en el offset
+%     [centro_x, centro_y, r_teorico, r_individual, centro_individual] = mido_patron_centros_coincidentes(frames_cilindro, id_cilindro{f}, px2mmPol, offset_fronteras, F, FC, path_datos); % haciendo coincidir los 2 centros
+    fprintf([id_cilindro{f} '\nError 2 cámaras: %.3f mm\nError C1: %.3f mm, Error C2: %.3f mm\nCentro global: (%.3f, %.3f)\nCentro C1: (%.3f, %.3f)\nCentro C2: (%.3f, %.3f)\n\n'], 2*r_teorico - nominales(f), 2*r_individual(1) - nominales(f), 2*r_individual(2) - nominales(f), centro_x, centro_y, centro_individual{1}(1), centro_individual{1}(2), centro_individual{2}(1), centro_individual{2}(2))
+end
+
+% longitudes media, mínima y máxima del trapecio considerando los errores
+% dados por la std:
+% l_min = norm(offset_fronteras(1:2) - offset_fronteras(3:4));
+% l_max = norm(offset_fronteras(1:2) + offset_fronteras(3:4));
+
+% fprintf('Longitud media trapecio: %.3f\n', norm(offset_fronteras))
+% fprintf('Longitud mínima trapecio: %.3f - Error: %.3f\n', l_min, l_min - norm(offset_fronteras))
+% fprintf('Longitud máxima trapecio: %.3f - Error: %.3f\n', l_max, l_max - norm(offset_fronteras))
---
+++ tesis/sin_tirar_bordes/mido_patron_2021.m
@@ -0,0 +1,143 @@
+function [centro_x, centro_y, r_teorico, r_individual, centro_individual] = mido_patron_2021(frames_cilindro, id_cilindro, px2mmPol, offset, F, FC, path_plot)
+
+    % id_cilindro es un str tipo '34700530'
+    % frames_cilindro es un cell de 2 casilleros que tiene en cada uno el
+    % directorio del frame de cada c�mara
+    
+    mc = {'ob', '.r'}; % markers para los cilindros
+    mf = {'--b', '--r'}; % markers para las fronteras
+    mfc = {'--c', '--m'}; % markers para las fronteras de calibracion
+        
+    %close all
+    h1=figure(1);hold on
+    XY = {[], []};
+    r_individual = nan(2,1);
+    centro_individual = {nan(2,1), nan(2,1)};
+    for q = 1:2
+
+        I=imread(frames_cilindro{q});
+        Iinfo=imfinfo(frames_cilindro{q});
+        if ~isempty(Iinfo.SignificantBits)
+            I=bitshift(I,Iinfo.SignificantBits-16);
+        elseif ~isempty(Iinfo.BitDepth)
+            I=double(I)/Iinfo.BitDepth;
+        end
+        I=double(I);
+        frame = I;
+
+        py = median(frame);
+        px = 1:size(py,2);
+        [px, py] = tiro_datos_nulos_perfil_2021(px, py);
+        px = px.';
+        py = py.';
+
+        py = 1088-py;
+
+        x = polyval4XY_2021(px2mmPol{q}(1), px, py);
+        y = polyval4XY_2021(px2mmPol{q}(2), px, py);
+        
+        % convierto a mm las fronteras dadas por la calibraci�n
+        temp_x = polyval4XY_2021(px2mmPol{q}(1), FC{q}(:,1), FC{q}(:,2));
+        temp_y = polyval4XY_2021(px2mmPol{q}(2), FC{q}(:,1), FC{q}(:,2));
+        FC{q} = [temp_x, temp_y];
+
+        % desplazar el 2do
+        if q == 2
+            x = x-offset(1);
+            y = y-offset(2);
+            
+            FC{q} = [FC{q}(:,1)-offset(1), FC{q}(:,2)-offset(2)];
+        end
+
+        % ahora que tengo definidos x,y, les aplico las fronteras
+        ind1 = inpolygon(x, y, F{q}(:,1), F{q}(:,2));
+        ind2 = inpolygon(x, y, FC{q}(:,1), FC{q}(:,2));
+%         ind1 = true(size(ind1)); % OJO! esto es para usar calibraciones viejas en las que no lo tengo definido
+        
+        ind = ind1&ind2;
+        
+        % para medir con calibraciones viejas necesito ignorar toda
+        % frontera
+%         ind = true(numel(x),1);
+        
+% 
+        % guardo los datos en una celda que junta las 2 c�maras
+        xy = [x(ind), y(ind)];
+        XY{q} = xy;
+        
+        %         plot(x,y,mc{q})
+        figure(1)
+        plot(XY{q}(:,1),XY{q}(:,2),mc{q})
+        plot(F{q}(:,1), F{q}(:,2), mf{q})
+        plot(FC{q}(:,1), FC{q}(:,2), mfc{q})
+        
+        % mart�n quiere ver cu�nto mide el radio calculado con cada c�mara
+        % por separado
+        circulo = TaubinNTN_2021(xy);
+        centro_x = circulo(1);
+        centro_y = circulo(2);
+        r_individual(q) = circulo(3);
+        centro_individual{q} = [centro_x, centro_y];
+        
+        error = r_individual(q) - sqrt((xy(:,1) - centro_x).^2 + (xy(:,2) - centro_y).^2);
+        h2=figure(2); hold on, grid on
+        plot(xy(:,1), error, '.')
+        xlabel('X (mm)')
+        ylabel('Error radial (mm)')
+        title(['C�mara ' num2str(q)])
+%         saveas(h2, [path_plot id_cilindro '_error_C' num2str(q) '.png'])
+        close(2)
+        
+        % calculo el �ngulo que ven las c�maras
+        P0 = [centro_x, centro_y];
+        P1 = [XY{q}(1,1), XY{q}(1,2)];
+        P2 = [XY{q}(end,1), XY{q}(end,2)];
+        angulo = calculo_angulo_2021(P1, P0, P2, P0);
+        fprintf('El �ngulo que ve C%d es %.0f�\n', q, angulo)
+
+    end
+    
+%     % calculo el �ngulo que ve C1
+%     P0 = [centro_x, centro_y];
+%     P1 = [XY{1}(1,1), XY{1}(1,2)];
+%     P2 = [XY{1}(end,1), XY{1}(end,2)];
+%     angulo = calculo_angulo(P1, P0, P2, P0);
+%     fprintf('El �ngulo que ve C1 es %.0f�\n', angulo)
+
+    % convierto la celda a matriz
+%     XY = [XY{1}, XY{2}];
+    XY = [XY{1}; XY{2}];
+
+    % veo qu� tan circular es, y mido el di�metro
+    circulo = TaubinNTN_2021(XY);
+
+    centro_x = circulo(1);
+    centro_y = circulo(2);
+    r_teorico = circulo(3);
+
+    t = linspace(0, 2*pi, 100)';
+    x_teorico = r_teorico * cos(t) + centro_x;
+    y_teorico = r_teorico * sin(t) + centro_y;
+
+    figure(1)
+    plot(centro_x, centro_y, '+r')
+    plot(x_teorico, y_teorico, '--k')
+
+    axis equal
+    grid on
+    xlabel('X (mm)')
+    ylabel('Y (mm)')
+    title(id_cilindro)
+    
+%     saveas(h1, [path_plot id_cilindro '_medicion.png'])
+
+    % calculo el residuo del ajuste y lo grafico
+    error = r_teorico - sqrt((XY(:,1) - centro_x).^2 + (XY(:,2) - centro_y).^2);
+    h3=figure(3); hold on, grid on
+    plot(XY(:,1), error, '.')
+    xlabel('X (mm)')
+    ylabel('Error radial (mm)')
+    title('2 c�maras combinadas')
+%     saveas(h3, [path_plot id_cilindro '_error_camaras_combinadas.png'])
+    
+end
